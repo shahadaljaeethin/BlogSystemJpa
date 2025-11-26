350aa2d9a4bcd7d8c07594bc68a5c79826bb84fb
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-11-26 10:43:34 +0300

    upd

---
+++ src/main/java/com/example/BlogSystem/Service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.BlogSystem.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+
+}
